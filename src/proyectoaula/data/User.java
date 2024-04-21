e9edac26dc4a041ba2fd48d44fe3e18a660f2ce2
MiguelDev <migopa0222@gmail.com>
2024-04-20 21:58:25 -0500

    Panel admin

---
+++ src/proyectoaula/data/User.java
@@ -0,0 +1,14 @@
+package proyectoaula.data;
+
+/**
+ *
+ * @author migopa
+ */
+public class User {
+    public String nombre;
+    public String carrera;        
+    public String numeroIdentificacion;
+    public String apellido;
+    public  int id;
+    public int puntos; 
+}
