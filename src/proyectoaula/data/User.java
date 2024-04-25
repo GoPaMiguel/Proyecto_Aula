19e86657d71d62d13744e7232a9a0d85422d7f5f
MiguelDev <migopa0222@gmail.com>
2024-04-25 16:36:32 -0500

    Crear un estudiante

--- src/proyectoaula/data/User.java
+++ src/proyectoaula/data/User.java
@@ -1,15 +1,17 @@
 package proyectoaula.data;
 
+import java.util.*;
+
 /**
  *
  * @author migopa
  */
 public class User {
-    public String nombre;
-    public String carrera;        
-    public String numeroIdentificacion;
-    public String apellido;
+    public  String nombre;//
+    public String carrera; //       
+    public String numeroIdentificacion;//
+    public String apellido;//
     public String genero;
-    public  int id;
-    public int puntos; 
+    public int puntos;
+    public static HashMap<String, User> usuarios;
 }
