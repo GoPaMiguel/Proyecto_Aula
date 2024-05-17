6f43ee4196d993925a2079220a5bc472c64e0a24
BreinerMontes01 <breiner.montespetro@unicolombo.edu.co>
2024-05-17 11:29:47 -0500

    Cambios de residuo

---
+++ src/proyectoaula/data/Residuos.java
@@ -0,0 +1,62 @@
+
+package proyectoaula.data;
+
+public class Residuos {
+    private String codigo;
+    private String material;
+    private String objeto;
+    private int puntos;
+    
+    public Residuos(){
+        
+    }
+
+    public Residuos(String codigo, String material, String objeto, int puntos) {
+        this.codigo = codigo;
+        this.material = material;
+        this.objeto = objeto;
+        this.puntos = puntos;
+}@Override
+    public String toString() {
+        return "Residuo{" +
+                "codigo='" + codigo + '\'' +
+                ", material='" + material + '\'' +
+                ", objeto='" + objeto + '\'' +
+                ", puntos=" + puntos +
+                '}';
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(String objeto) {
+        this.objeto = objeto;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+    
+    
+}
