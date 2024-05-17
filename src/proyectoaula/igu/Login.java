7b572ff28539a37f2c635ecd8b1418c21ae2bcab
MiguelDev <migopa0222@gmail.com>
2024-05-17 15:43:58 -0500

    update cofig

--- src/proyectoaula/igu/Login.java
+++ src/proyectoaula/igu/Login.java
@@ -183,7 +183,6 @@ public class Login extends javax.swing.JFrame {
 
     private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
         // TODO add your handling code here:
-
         String usuario = FieldUser.getText();
         String contraseña = FieldPassword.getText();
 
